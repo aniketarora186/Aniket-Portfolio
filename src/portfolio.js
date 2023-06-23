f5a63e007beb19328f9bbb5055b8f5e489374519
Manav Arora <aroramanav887@gmail.com>
2023-06-23 16:27:28 +0530

    initial-commit

---
+++ src/portfolio.js
@@ -0,0 +1,304 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//SEO Related settings
+const seo = {
+  title: "Aniket's Portfolio",
+  description:
+    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  og: {
+    title: "Aniket Arora Portfolio",
+    type: "website",
+  },
+};
+
+//Home Page
+const greeting = {
+  title: "Aniket Arora",
+  logo_name: "Aniket Arora",
+  nickname: "",
+  subTitle:
+    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  resumeLink:
+    "https://drive.google.com/file/d/1lWHn5BOwMvLdx5RCvH9yjs_CiyGIgT8t/view?usp=drivesdk",
+  portfolio_repository: "",
+  githubProfile: "https://github.com/aniketarora186",
+};
+
+const socialMediaLinks = [
+
+  {
+    name: "Github",
+    link: "https://github.com/aniketarora186",
+    fontAwesomeIcon: "fa-github", 
+    backgroundColor: "#181717",
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/aniketarora98/",
+    fontAwesomeIcon: "fa-linkedin-in", 
+    backgroundColor: "#0077B5", 
+  },
+  
+  {
+    name: "Gmail",
+    link: "mailto:9893107186annu@gmail.com",
+    fontAwesomeIcon: "fa-google", 
+    backgroundColor: "#D14836", 
+  },
+ 
+ 
+  {
+    name: "Instagram",
+    link: "https://instagram.com/the_aniketarora?igshid=ZDc4ODBmNjlmNQ==",
+    fontAwesomeIcon: "fa-instagram", 
+    backgroundColor: "#E4405F",
+  },
+];
+
+const skills = {
+  data: [
+  
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building responsive website front end using React-Redux",
+        "⚡ Creating application backend in Node & Express.",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "Sass",
+          fontAwesomeClassname: "simple-icons:sass",
+          style: {
+            color: "#CC6699",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+       
+       
+        
+      ],
+    },
+  
+  ],
+};
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "LeetCode",
+      iconifyClassname: "simple-icons:leetcode",
+      style: {
+        color: "#F79F1B",
+      },
+      profileLink: "https://leetcode.com/0901CA221013/",
+    },
+    {
+      siteName: "HackerRank",
+      iconifyClassname: "simple-icons:hackerrank",
+      style: {
+        color: "#2EC866",
+      },
+      profileLink: "https://www.hackerrank.com/9893107186annu?hr_r=1",
+    },
+   
+   
+    {
+      siteName: "Hackerearth",
+      iconifyClassname: "simple-icons:hackerearth",
+      style: {
+        color: "#323754",
+      },
+      profileLink: "https://www.hackerearth.com/@9893107186annu",
+    },
+   
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Madhav Institue of Technology and Science, Gwalior",
+      subtitle: "Masters in Computer Applications",
+      logo_path: "mits.png",
+      alt_name: "",
+      duration: "2022 - 2024",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Artificial Intelligence, Blockchain Technology, Cloud Computing and Full Stack Development.",
+        
+      ],
+      website_link: "",
+    },
+    {
+      title: "Govt. Adarsh Science College, Gwalior",
+      subtitle: "Bachelors of Science",
+      logo_path: "college.jpg",
+      alt_name: "",
+      duration: "2017 - 2020",
+      descriptions: [
+        "⚡ Selected as INSPIRE SCHOLAR by Deptt. of Science and Technology, Govt. of India.",
+        
+      ],
+      website_link: "",
+    },
+
+    {
+      title: "Sant Kanwar Ram Higher Secondary School",
+      subtitle: "Schooling",
+      logo_path: "skr.jpeg",
+      alt_name: "",
+      duration: "2015 - 2017",
+      descriptions: [
+        "⚡ Selected in Madhya Pradesh Mission Excellence 2015 by Deptt. of Science and Technology, Govt. of Madhya Pradesh.",
+        "⚡ Madhya Pradesh State Topper 2017 in Higher Secondary Examination.",
+        
+      ],
+      website_link: "",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+  
+    
+   
+    {
+      title: "Artificial Intelligence",
+      subtitle: "",
+      logo_path: "accenture.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1idS9xjKA215DwpJlzU6lFyXJl07DmeJb/view?usp=drivesdk",
+      alt_name: "",
+      color_code: "white",
+    },
+    {
+      title: "Master Data Management",
+      subtitle: "",
+      logo_path: "tcs.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1lD5im9l6b3yzw9U_M62qrKxEszTmgYMP/view?usp=drivesdk",
+      alt_name: "",
+      color_code: "#FB6376",
+    },
+  
+    {
+      title: "Blockchain and its Applications",
+      subtitle: "",
+      logo_path: "nptel_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1k2134QIeHoU2tXLQgGkuQVgqngUmdLDr/view?usp=drivesdk",
+      alt_name: "",
+      color_code: "#FFBB0099",
+    },
+   
+  ],
+};
+
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Web Based Applications.",
+  avatar_image_path: "projects_image.svg",
+};
+
+
+
+
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "Aniket.jpg",
+    description:
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with Full Stack Web Development.",
+  },
+  // blogSection: {
+  //   title: "Blogs",
+  //   subtitle:
+  //     "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
+  //   link: "https://blogs.ashutoshhathidara.com/",
+  //   avatar_image_path: "blogs_image.svg",
+  // },
+  addressSection: {
+    title: "Address",
+    subtitle:
+      "Kamleshwar Colony, Dabra, Distt. Gwalior, Madhya Pradesh- 475110",
+    locality: "Dabra",
+    country: "IN",
+    region: "Madhya Pradesh",
+    postalCode: "475110",
+    streetAddress: "Kamleshwar Colony",
+    avatar_image_path: "address_image.svg",
+    location_map_link: "",
+  },
+  phoneSection: {
+    title: "Mobile Number",
+    subtitle: "9893107186",
+  },
+};
+
+export {
+  settings,
+  seo,
+  greeting,
+  socialMediaLinks,
+  skills,
+  competitiveSites,
+  degrees,
+  certifications,
+  projectsHeader,
+  contactPageData,
+};
