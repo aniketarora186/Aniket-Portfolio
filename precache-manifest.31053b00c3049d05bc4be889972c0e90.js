a6ae707ebaf6897f692e5786c37b5a87f3bc0750
Manav Arora <aroramanav887@gmail.com>
2023-06-23 17:30:51 +0530

    Updates

---
+++ precache-manifest.31053b00c3049d05bc4be889972c0e90.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018d3fa915933db0ff3d63a4076eba08",
+    "url": "/Aniket-Portfolio/index.html"
+  },
+  {
+    "revision": "7ceb6e183412e2dc0825",
+    "url": "/Aniket-Portfolio/static/css/main.398922ae.chunk.css"
+  },
+  {
+    "revision": "db6970ba45503a849335",
+    "url": "/Aniket-Portfolio/static/js/2.92ca2142.chunk.js"
+  },
+  {
+    "revision": "7ceb6e183412e2dc0825",
+    "url": "/Aniket-Portfolio/static/js/main.32481ba0.chunk.js"
+  },
+  {
+    "revision": "bd8ea78dc93b94d88251",
+    "url": "/Aniket-Portfolio/static/js/runtime-main.f6d96657.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Aniket-Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "0fbc0f079b96d0e13824d44b8df839c3",
+    "url": "/Aniket-Portfolio/static/media/Aniket.0fbc0f07.jpg"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/Aniket-Portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/Aniket-Portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/Aniket-Portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/Aniket-Portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/Aniket-Portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/Aniket-Portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Aniket-Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/Aniket-Portfolio/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/Aniket-Portfolio/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/Aniket-Portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/Aniket-Portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/Aniket-Portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/Aniket-Portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/Aniket-Portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/Aniket-Portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/Aniket-Portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/Aniket-Portfolio/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/Aniket-Portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/Aniket-Portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/Aniket-Portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/Aniket-Portfolio/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/Aniket-Portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/Aniket-Portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "d71b8474a613f47ed1d542de8da66702",
+    "url": "/Aniket-Portfolio/static/media/mits.d71b8474.png"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/Aniket-Portfolio/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "5f4a6393d3d17fe0ead752db88871396",
+    "url": "/Aniket-Portfolio/static/media/tcs.5f4a6393.png"
+  }
+]);
